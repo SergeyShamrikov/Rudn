5cd6a87de853d775abba6f0646744c9e88e7015e
seliseeva <wingart.eliseeva@gmail.com>
2014-05-21 13:03:32 +0300

    правки emplo

---
+++ about_employee.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta charset="utf-8">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+   <script type="text/javascript" src="js/jquery.flexslider.js"></script>
+	 <script type="text/javascript" src="js/owl.carousel.js"></script>
+	 <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	 <script src="js/script.js"></script>
+	 
+	<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+    <!--[if lt IE 9]>
+   	<script type="text/javascript" src="js/html5.js"></script>
+    	<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+</head>
+<body>
+<!--==============================header=================================-->
+    <?php include("blocks/header.php") ?>
+<!--==============================content================================-->
+    <div id="content">
+      <?php include("components/make_donation.php") ?>
+      <div class="main">
+        <?php include("components/menu_content.php") ?>
+        <h4 class="title2 museo_font900 employee_title1">Руководство</h4> 
+      <div class="wrapper">
+        <div class="f_left employee_left">
+          <div class="employee_left_inner">
+            <figure class="al_center">
+              <img src="images/rector_type.png" alt="picture">
+              <figcaption>
+                <h4 class="title3 museo_font900">Филиппов Владимир Михайлович</h4>
+                <span class="employee_text_type1">Ректор РУДН. Доктор физико-математических наук, профессор.</span>
+                <span class="employee_text_type2">Министр образования РФ с 1998 по 2004 год. Академик Российской академии образования.</span>
+                <span class="employee_text_type2">Выпускник факультета физико-математических и естественных наук 1973 года.</span>
+                <a href="#javascript:;" class="d_block"> &lt; Вернуться</a>
+              </figcaption>
+            </figure>
+          </div>
+         
+        </div>
+        <div class="wrapper employee_right employee_text_type3">
+          <p>В.М. Филиппов родился 15 апреля 1951 года в городе Урюпинске Сталинградской области;  отец – военнослужащий, мать -  медицинская сестра.</p>
+          <p>В 1958-1968гг. учился в средней школе №2 имени А.С. Пушкина, г. Урюпинска, где с 4 класса был отличником учебы, занимался активно спортом и был лидером школьных организаций. Среднюю школу закончил с серебряной медалью.</p>
+          <p>В 1968-1973 гг. – студент  Университета дружбы народов имени Патриса Лумумбы, окончил факультет физико-математических и естественных наук с  отличием по специальности «математика».</p>
+          <p>1973-75 гг. – аспирант Университета.</p>
+          <p>1975-76 гг. – Служба в Вооруженных Силах СССР.</p>
+          <p>1976-79 гг. – Председатель Совета молодых ученых Университета.</p>
+          <p>1979-80 гг. – ассистент кафедры высшей математики Университета.</p>
+          <p>1980г.  – защита кандидатской диссертации в Математическом институте им. В.А. Стеклова АН СССР, по специальности 01.01.01 – «Математический анализ», в математической школе академика Никольского С.М. (научный руководитель – член-корреспондент АН СССР, проф. Кудрявцев Л.Д.).</p>
+          <p>1980-87 гг. –  начальник Научного управления Университета.</p>
+          <p>1983-84 гг. – командировка для научной работы в области математики в Свободном Университете Брюсселя (Бельгия).</p>
+          <p>1985-2000 гг. – заведующий кафедрой математического анализа Российского университета дружбы народов.</p>
+          <p>1986г. – защита докторской диссертации в Математическом институте им. В.А. Стеклова АН СССР по специальности 01.01.01 – «математический анализ».</p>
+          <p>1987г. - присвоено ученое звание «профессор по кафедре математического анализа».</p>
+          <p>Февраль 1987 – декабрь 1989гг. – секретарь партийного комитета Университета дружбы народов.</p>
+          <p> Декабрь 1989 – июнь 1993 гг. – декан факультета физико-математических и естественных наук РУДН.</p>
+        </div>
+      </div>
+
+       </div>
+    </div>
+ 
+
+<!--==============================footer=================================-->
+     <?php include("blocks/footer.php") ?>
+    
+</body>
+</html>
