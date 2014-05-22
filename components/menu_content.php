5a18cd3b935ce3af9f144256682db0d558491752
seliseeva <wingart.eliseeva@gmail.com>
2014-05-22 12:06:04 +0300

    Merge branch 'master' of https://github.com/SergeyShamrikov/Rudn
    Conflicts:
    	banking_details.php

--- components/menu_content.php
+++ components/menu_content.php
@@ -1,18 +1,18 @@
 <section class="menu_content_box clearfix">
 	<div class="menu_content_box_it1 f_left clearfix current">
-		<a href="#">
+		<a class="menu_content_box_lk" href="banking_details.php">
 			<figure class="f_left menu_content_img"></figure>
 			<h6 class="menu_content_title wrapper">Реквизиты фонда<span class="d_block">Перейти на страницу</span></h6>
 		</a>
 	</div>
 	<div class="menu_content_box_it2 f_left clearfix">
-		<a href="reports.php">
+		<a class="menu_content_box_lk" href="reports.php">
 			<figure class="f_left menu_content_img"></figure>
 			<h6 class="menu_content_title wrapper">Отчетность фонда<span class="d_block">Перейти на страницу</span></h6>
 		</a>
 	</div>
 	<div class="menu_content_box_it3 f_left clearfix">
-		<a href="leadership.php">
+		<a class="menu_content_box_lk" href="leadership.php">
 			<figure class="f_left menu_content_img"></figure>
 			<h6 class="menu_content_title wrapper">Руководство фонда<span class="d_block">Перейти на страницу</span></h6>
 		</a>
