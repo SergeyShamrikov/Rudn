a283e4455442a344ed9d0fd3e1d39e2b9a002845
seliseeva <wingart.eliseeva@gmail.com>
2014-05-22 17:19:36 +0300

    баги по блоку make_donation_baner

--- components/make_donation.php
+++ components/make_donation.php
@@ -1,5 +1,5 @@
 <section class="make_donation_baner p_rel">
 	<a href="make_donation.php" class="d_block">
-		<div class="make_donation_baner_title d_in-block p_rel">Сделать взнос</div>
+		<span class="make_donation_baner_title d_in-block p_rel">Сделать взнос</span>
 	</a>
 </section>
