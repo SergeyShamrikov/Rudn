90474f0f814d7ba1da5664056f28942ef313db48
seliseeva <wingart.eliseeva@gmail.com>
2014-05-22 09:32:04 +0300

    banking_details

---
+++ banking_details.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Datails_banking</title>
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta charset="utf-8">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+   <script type="text/javascript" src="js/jquery.flexslider.js"></script>
+	 <script type="text/javascript" src="js/owl.carousel.js"></script>
+	 <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	 <script src="js/script.js"></script>
+	 
+	<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+    <!--[if lt IE 9]>
+   	<script type="text/javascript" src="js/html5.js"></script>
+    	<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+</head>
+<body>
+<!--==============================header=================================-->
+    <?php include("blocks/header.php") ?>
+<!--==============================content================================-->
+    <div id="content">
+      <?php include("components/make_donation.php") ?>
+      <div class="main">
+       <?php include("components/menu_content.php") ?>
+       <h4 class="title2 museo_font900">Реквизиты</h4>
+       <div class="banking_box wrapper">
+          <div class="f_left al_left bankdetails_box">
+            <h4 class="title6">Платежные реквизиты для перечисления денежных средств в ЕВРО: </h4>
+            <ul>
+              <li>BANK NAME: Sberbank (Head office-all branches and offices in Russia) Moscow </li>
+              <li>SWIFT: SABR RU MM </li>
+              <li>BENIFICIARY ACCOUNT: 40503978638110200323 </li>
+              <li>BENIFICIARY: Peoples’ Friendship University of Russia </li>
+              <li>ADDRESS: Miklukho-Maklay Str., 6, Moscow, Russia, 117198 </li>
+              <li>CORRESPONDENT ACCOUNT: № 10094987261000; Deutsche Bank AG, Frankfurt am Main (SWIFT-DEUTDEFF) </li>
+            </ul>
+            <span class="d_block">В назначении платежа обязательно указать договор пожертвования № 2 от 01.02.2014 </span>
+            <a href="#javascript:;" class="details d_in-block">Печать</a>
+          </div>
+         <img src="images/banking_details1.png" class="f_right banking_img" alt="picture">
+       </div>
+
+       <div class="banking_box wrapper">
+          <div class="f_left al_left bankdetails_box">
+            <h4 class="title6">Платежные реквизиты для перечисления денежных средств в  Долларах США: </h4>
+            <ul>
+              <li>ANK NAME: Sberbank (Head office-all branches and offices in Russia) Moscow  </li>
+              <li>SWIFT: SABR RU MM </li>
+              <li>BENIFICIARY ACCOUNT: 40503840038110200323  </li>
+              <li>BENIFICIARY: Peoples’ Friendship University of Russia </li>
+              <li>ADDRESS: Miklukho-Maklay Str., 6, Moscow, Russia, 117198 </li>
+              <li>CORRESPONDENT ACCOUNT: № 8900057610 in the Bank of New York Mellon, New York (SWIFT – IRVT US 3N)  </li>
+            </ul>
+            <span class="d_block">В назначении платежа обязательно указать договор пожертвования № 2 от 01.02.2014 </span>
+            <a href="#javascript:;" class="details d_in-block">Печать</a>
+          </div>
+         <img src="images/banking_details1.png" class="f_right banking_img" alt="picture">
+       </div>
+
+       <div class="banking_box wrapper">
+          <div class="f_left al_left bankdetails_box">
+            <h4 class="title6">Платежные реквизиты для перечисления денежных средств в Рублях РФ: </h4>
+            <ul>
+              <li>Банк получателя: В Отделение 1 Москва</li>
+              <li>ИНН 7728073720 </li>
+              <li>КПП 772801001</li>
+              <li>Получатель: УФК по г.Москве (л/сч 20736X13430 РУДН)</li>
+              <li>БИК 044583001</li>
+              <li>Счет № 40501810600002000079</li>
+              <li>КБК 00000000000000000180</li>
+              <li>ОКТМО 45905000 </li>
+            </ul>
+            <span class="d_block">В назначении платежа обязательно указать договор пожертвования № 2 от 01.02.2014 </span>
+            <a href="#javascript:;" class="details d_in-block">Печать</a>
+          </div>
+         <img src="images/banking_details1.png" class="f_right banking_img" alt="picture">
+       </div>
+      </div>
+    </div>
+<!--==============================footer=================================-->
+     <?php include("blocks/footer.php") ?>
+    
+  </body>
+</html>
