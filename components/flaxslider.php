c95ab8948b260533298f08892cd9f34adc632ec5
SergeyShamrikov <shamrikov.01@gmail.com>
2014-05-26 09:24:37 +0300

    правка флекслайдера

--- components/flaxslider.php
+++ components/flaxslider.php
@@ -1,62 +1,62 @@
 <div class="flexslider wrapper">
 	<ul class="slides">
-		<li class="slides_it p_rel 1">
-			<img class="slides_it_img" src="images/flexslider_img1.png" alt="">
-			<div class="p_abs flexslider_box">
-				<div class="main clearfix">
-					<div class="f_right flax_banner">
-						<figure>
-							<img src="images/logo_big.png" alt="">
-						</figure>
-						<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
-						<a href="#" class="f_right details">Подробнее</a>
-						<div class="clear"></div>
-					</div>
+		<li class="slides_it p_rel">
+			<div class="main clearfix">
+				<div class="f_right flax_banner">
+					<figure>
+						<img src="images/logo_big.png" alt="">
+					</figure>
+					<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
+					<a href="#" class="f_right details">Подробнее</a>
+					<div class="clear"></div>
 				</div>
 			</div>
 		</li>
-		<li class="slides_it p_rel 2">
-			<img class="slides_it_img" src="images/flexslider_img1.png" alt="">
-			<div class="p_abs flexslider_box">
-				<div class="main clearfix">
-					<div class="f_right flax_banner">
-						<figure>
-							<img src="images/logo_big.png" alt="">
-						</figure>
-						<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
-						<a href="#" class="f_right details">Подробнее</a>
-						<div class="clear"></div>
-					</div>
+		<li class="slides_it p_rel">
+			<div class="main clearfix">
+				<div class="f_right flax_banner">
+					<figure>
+						<img src="images/logo_big.png" alt="">
+					</figure>
+					<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
+					<a href="#" class="f_right details">Подробнее</a>
+					<div class="clear"></div>
 				</div>
 			</div>
 		</li>
-		<li class="slides_it p_rel 3">
-			<img class="slides_it_img" src="images/flexslider_img1.png" alt="">
-			<div class="p_abs flexslider_box">
-				<div class="main clearfix">
-					<div class="f_right flax_banner">
-						<figure>
-							<img src="images/logo_big.png" alt="">
-						</figure>
-						<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
-						<a href="#" class="f_right details">Подробнее</a>
-						<div class="clear"></div>
-					</div>
+		<li class="slides_it p_rel">
+			<div class="main clearfix">
+				<div class="f_right flax_banner">
+					<figure>
+						<img src="images/logo_big.png" alt="">
+					</figure>
+					<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
+					<a href="#" class="f_right details">Подробнее</a>
+					<div class="clear"></div>
 				</div>
 			</div>
 		</li>
-		<li class="slides_it p_rel 4">
-			<img class="slides_it_img" src="images/flexslider_img1.png" alt="">
-			<div class="p_abs flexslider_box">
-				<div class="main clearfix">
-					<div class="f_right flax_banner">
-						<figure>
-							<img src="images/logo_big.png" alt="">
-						</figure>
-						<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
-						<a href="#" class="f_right details">Подробнее</a>
-						<div class="clear"></div>
-					</div>
+		<li class="slides_it p_rel">
+			<div class="main clearfix">
+				<div class="f_right flax_banner">
+					<figure>
+						<img src="images/logo_big.png" alt="">
+					</figure>
+					<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
+					<a href="#" class="f_right details">Подробнее</a>
+					<div class="clear"></div>
+				</div>
+			</div>
+		</li>
+		<li class="slides_it p_rel">
+			<div class="main clearfix">
+				<div class="f_right flax_banner">
+					<figure>
+						<img src="images/logo_big.png" alt="">
+					</figure>
+					<p class="flax_banner_text">Наиболее прозрачный инструмент благотворительности, при котором деньги жертвователей не расходуются, а постоянно работают на благо образования,</p>
+					<a href="#" class="f_right details">Подробнее</a>
+					<div class="clear"></div>
 				</div>
 			</div>
 		</li>
