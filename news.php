0f6ec655d6b0963e9a981714890e10ffb106b06b
SergeyShamrikov <shamrikov.01@gmail.com>
2014-06-03 14:51:45 +0300

    страници новости+события

---
+++ news.php
@@ -0,0 +1,209 @@
+<html lang="en">
+<head>
+    <title>Новости</title>
+     <?php include("blocks/head.php") ?>
+	 
+	 
+	<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+    <!--[if lt IE 9]>
+   	<script type="text/javascript" src="js/html5.js"></script>
+    	<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+</head>
+<body>
+<!--==============================header=================================-->
+   	  <?php include("blocks/header.php") ?><!--==============================content================================-->
+  <div class="main">
+    <?php include("components/make_donation.php") ?>
+    <section class="news_box">
+   	<div class="main">
+   		<h2 class="news_title f_left">Новости</h2>
+      <div class="filter">
+        <ul class="filter_list wrapper">
+            <li class="filter_it1">
+                <a href="#" class="filter_lk_arrow"><img src="images/news_arrow_left.png" alt=""></a>
+            </li>
+            <li class="filter_it">
+                <a href="#" class="filter_lk">1</a>
+            </li>
+            <li class="filter_it activ">
+                <a href="#" class="filter_lk">2</a>
+            </li>
+            <li class="filter_it">
+                <a href="#" class="filter_lk">3</a>
+            </li>
+            <li class="filter_it">
+                <a href="#" class="filter_lk_arrow"><img src="images/news_arrow_right.png" alt=""></a>
+            </li>
+        </ul>
+      </div>
+   	</div>
+   	<div class="box_news">
+   		<div class="news_box1 wrapper">
+   			<div class="owl_carousel_it">
+   					<figure class="p_rel img_box wrapper">
+   						<a href="#" class="d_block">
+   							<img src="images/carousel_img1.jpg" alt="">
+   							<div class="img_box_title p_abs wrapper">
+   								<span class="">Выпускник ИИЯ РУДН Сидор Николаевич Пак снова в стенах университета.</span>
+   							</div>
+   						</a>
+   					</figure>
+   					<figcaption class="img_descrition">
+   						<p>7 мая ИИЯ РУДН посетил выпускник Института С.Н.Пак – доверенное лицо Президента РФ В.В области.</p>
+   						<time>08.05.2014</time>
+   					</figcaption>
+   				</div>
+          <div class="owl_carousel_it">
+   					<figure class="p_rel img_box wrapper">
+   						<a href="#" class="d_block">
+   							<img src="images/carousel_img2.jpg" alt="">
+   							<div class="img_box_title p_abs wrapper">
+   								<span class="">Международная научно-практическая</span>
+   							</div>
+   						</a>
+   					</figure>
+   					<figcaption class="img_descrition">
+   						<p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+   						<time>08.05.2014</time>
+   					</figcaption>
+   				</div>
+          <div class="owl_carousel_it">
+   					<figure class="p_rel img_box wrapper">
+   						<a href="#" class="d_block">
+   							<img src="images/carousel_img3.jpg" alt="">
+   							<div class="img_box_title p_abs wrapper">
+   								<span class="">Цикл встреч с выпускниками экологического факультета.</span>
+   							</div>
+   						</a>
+   					</figure>
+   					<figcaption class="img_descrition">
+   						<p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+   						<time>08.05.2014</time>
+   					</figcaption>
+   				</div>
+   		 </div>
+      <div class="news_box2 wrapper">
+        <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img1.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Выпускник ИИЯ РУДН Сидор Николаевич Пак снова в стенах университета.</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>7 мая ИИЯ РУДН посетил выпускник Института С.Н.Пак – доверенное лицо Президента РФ В.В области.</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+          <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img2.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Международная научно-практическая</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+          <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img3.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Цикл встреч с выпускниками экологического факультета.</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+              <time>08.05.2014</time>
+            </figcaption>
+        </div>
+       </div>
+      <div class="news_box3 wrapper">
+        <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img1.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Выпускник ИИЯ РУДН Сидор Николаевич Пак снова в стенах университета.</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>7 мая ИИЯ РУДН посетил выпускник Института С.Н.Пак – доверенное лицо Президента РФ В.В области.</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+          <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img2.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Международная научно-практическая</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+          <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img3.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Цикл встреч с выпускниками экологического факультета.</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+       </div>
+      </div>
+      <div class="filter">
+        <ul class="filter_list2 wrapper">
+            <li class="filter_it1">
+                <a href="#" class="filter_lk_arrow"><img src="images/news_arrow_left.png" alt=""></a>
+            </li>
+            <li class="filter_it">
+                <a href="#" class="filter_lk">1</a>
+            </li>
+            <li class="filter_it activ">
+                <a href="#" class="filter_lk">2</a>
+            </li>
+            <li class="filter_it">
+                <a href="#" class="filter_lk">3</a>
+            </li>
+            <li class="filter_it">
+                <a href="#" class="filter_lk_arrow"><img src="images/news_arrow_right.png" alt=""></a>
+            </li>
+        </ul>
+      </div>
+    </div>  
+   </section> 
+  </div>    
+
+
+<!--==============================footer=================================-->
+    <?php include("blocks/footer.php") ?>  
+
+</body>
+</html>
---
+++ news_1.php
@@ -0,0 +1,131 @@
+<html lang="en">
+<head>
+    <title>Новости</title>
+    <?php include("blocks/head.php") ?>
+	 
+	 
+	<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+    <!--[if lt IE 9]>
+   	<script type="text/javascript" src="js/html5.js"></script>
+    	<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+</head>
+<body>
+<!--==============================header=================================-->
+   	  
+    <?php include("blocks/header.php") ?>
+  <!--==============================content================================-->
+  <div class="main">
+    <?php include("components/make_donation.php") ?>
+
+    <section class="news_box">
+   	<div class="main">
+   		<h2 class="news1_title"><a href="#">Новости</a> / <span>Выпускник ИИЯ РУДН Сидор Николаевич Пак сновав стенах университета.</span></h2>
+   	</div>
+    <div class="one_news wrapper">
+      <div class="one_news_top wrapper">
+        <img src="images/news_1_img.png" alt="">
+          <h2 class="one_news_top_title">Выпускник ИИЯ РУДН Сидор Николаевич Пак снова<br> в стенах университета.</h2>
+        <div class="news_text">
+          <p class="one_news_top_text">Научно-методический семинар «Актуальные проблемы социальной педагогики,<br> психологии и управления развитием человеческих ресурсов общества (социального<br> менеджмента)»
+          <br>23 мая в ИИЯ на кафедре социальной педагогики состоялось очередное заседание<br> научно-методического семинара «Актуальные проблемы социальной педагогики,<br> психологии и управления развитием человеческих ресурсов общества (социального<br> менеджмента)». 
+          </p>
+        </div>  
+      </div>
+      <div class="one_news_bottom wrapper">
+        <div class="news_share wrapper">
+          <ul class="news_social_icons">
+            <li class="news_social_icons_item">
+              Поделится:
+            </li>
+            <li class="news_social_icons_item">
+              <a href="#">
+                <img src="images/news_social_icons_f.png" alt="">
+              </a>  
+              </li>
+            <li class="news_social_icons_item">
+              <a href="#">  
+                <img src="images/news_social_icons_vk.png" alt="">
+              </a>  
+              </li>
+            <li class="news_social_icons_item">
+              <a href="#">  
+                <img src="images/news_social_icons_twiit.png" alt="">
+              </a>  
+            </li>
+          </ul>
+        </div>
+        <div class="one_news_bottom_text">
+          <p>23 мая в ИИЯ на кафедре социальной педагогики состоялось очередное заседание научно-методического семинара «Актуальные проблемы<br> социальной педагогики, психологии и управления развитием человеческих ресурсов общества (социального менеджмента)». С докладом на тему <br>«Сокращение аудиторной нагрузки. Научно-методические проблемы сохранения качества образования» выступил старший преподаватель кафедры<br> социальной педагогики ИИЯ РУДН, кандидат педагогических наук  О.Д. Никитин. 
+          <br>
+          Научно-методический семинар «Актуальные проблемы социальной педагогики, психологии и управления развитием человеческих ресурсов общества<br> (социального менеджмента)»
+          <br>
+          23 мая в ИИЯ на кафедре социальной педагогики состоялось очередное заседание научно-методического семинара «Актуальные проблемы<br> социальной педагогики, психологии и управления развитием человеческих ресурсов общества (социального менеджмента)». 
+          <br>
+          23 мая в ИИЯ на кафедре социальной педагогики состоялось очередное заседание научно-методического семинара «Актуальные проблемы<br> социальной педагогики, психологии и управления развитием человеческих ресурсов общества (социального менеджмента)». 
+          </p>
+        </div>
+      </div>
+    </div>
+   	<div class="carousel_box_news">
+      <h2 class="one_news_bottom_title">Похожие новости</h2>
+   		<div class="news_box3 wrapper">
+        <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img1.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Выпускник ИИЯ РУДН Сидор Николаевич Пак снова в стенах университета.</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>7 мая ИИЯ РУДН посетил выпускник Института С.Н.Пак – доверенное лицо Президента РФ В.В области.</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+          <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img2.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Международная научно-практическая</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+          <div class="owl_carousel_it">
+            <figure class="p_rel img_box wrapper">
+              <a href="#" class="d_block">
+                <img src="images/carousel_img3.jpg" alt="">
+                <div class="img_box_title p_abs wrapper">
+                  <span class="">Цикл встреч с выпускниками экологического факультета.</span>
+                </div>
+              </a>
+            </figure>
+            <figcaption class="img_descrition">
+              <p>В этом году конференция будет посвящена следующим вопросам: проблемам межкультурного</p>
+              <time>08.05.2014</time>
+            </figcaption>
+          </div>
+       </div>
+      </div>
+    </div>  
+   </section> 
+  </div>    
+
+
+<!--==============================footer=================================-->
+    <?php include("blocks/footer.php") ?> 
+
+</body></html>
